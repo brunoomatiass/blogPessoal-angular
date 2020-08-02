281b4d0bd2ed8c731888e1cc33b120ea0c7c495f
unknown <brunomatias3@gmail.com>
2020-08-02 14:02:10 -0300

    Criação de novo projeto; instalação de bootstrap e auxiliares;  criação de componentes; rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FeedComponent } from './feed/feed.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'feed', component: FeedComponent},
+  {path: 'navbar', component: NavbarComponent},
+  {path: 'footer', component: FooterComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
